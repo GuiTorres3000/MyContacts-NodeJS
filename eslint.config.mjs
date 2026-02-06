0b24ec947a628a89cd502d30447ae11d62f9ee5d
GuiTorres3000 <guilhermetorres3000@gmail.com>
2026-02-06 09:18:10 -0300

    chore: eslint config with airbnb base

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,22 @@
-import js from "@eslint/js";
-import globals from "globals";
-import json from "@eslint/json";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import importPlugin from 'eslint-plugin-import';
+import airbnbBase from 'eslint-config-airbnb-base';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
-  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: {
+      js,
+      import: importPlugin,
+    },
+    languageOptions: {
+      globals: globals.node,
+    },
+    rules: {
+      ...airbnbBase.rules,
+      'quotes': ['error', 'single'],
+    },
+  },
 ]);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (request, response) => {
-    response.send("Hello World!");
+app.get('/', (request, response) => {
+    response.send('Hello World!');
 });
 
-app.listen(3333, () => console.log("Server started on port 3333"));
+app.listen(3333, () => console.log('Server started on port 3333'));
